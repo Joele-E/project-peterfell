ff597ea890d77722c47ab875f93d7c27f4fd90fa
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-08-04 19:36:53 +0200

    feat: component homepage navbar tailwind

---
+++ components/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#e09c9e",
+        cBlue: "#7a99ac",
+      },
+      fontFamily: {
+        Heebo: ["Heebo", "sans-serif"],
+        PlayfairSerif: ["Playfair Display", "serif"],
+        PlayfairSans: ["Playfair Display", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
