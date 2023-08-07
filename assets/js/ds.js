597cf0de1b1e9c49aba5633b4c0330372bca5de0
Joele-E <esposito.joelebiz@gmail.com>
2023-08-07 13:26:48 +0200

    feat: added design tool buttons to design system with copy element function on click

--- assets/js/ds.js
+++ assets/js/ds.js
@@ -4,10 +4,10 @@ function copyColor(id) {
   let bgColor = window.getComputedStyle(colorDiv).backgroundColor;
   colorDiv.navigator.clipboard.writeText(bgColor);
   console.log(`Copiato testo: ${bgColor}`);
+  colorDiv.no;
 }
 
 function copyElement(element) {
-  let classes = element.classList;
   navigator.clipboard.writeText(String(element.outerHTML));
   console.log(element);
 }
