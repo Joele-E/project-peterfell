c73a5764cc358d2179bb188610df2dc9903a57a3
AntonioIannibelli31 <antonioiannibelli31@gmail.com>
2023-08-30 17:45:45 +0200

    feat: added step2

--- components/homepage-demo/assets/js/home.js
+++ components/homepage-demo/assets/js/home.js
@@ -77,46 +77,69 @@ class DesignTool {
   step;
   currentStep;
   constructor() {
-    this.steps = {
-      1: "../component-step1/index.html",
-      2: "../step2/index.html",
-    };
+    this.steps = ["../component-step1/index.html", "../step2/index.html"];
     this.currentStep = 1;
   }
 
-  fetchPage() {
+  openDesignTool = () => {
     // console.log(this.steps[`${this.currentStep}`]);
-    fetch("../component-step1/index.html")
+    fetch(this.steps[this.currentStep - 1])
       .then((res) => res.text())
       .then((myPage) => {
         let parser = new DOMParser();
         let doc = parser.parseFromString(myPage, "text/html");
         let div = document.createElement("div");
+        div.id = "container-step";
         let content = doc.querySelector("body").outerHTML;
-        let btn1 = doc.getElementById("btn-1");
-        btn1.addEventListener("click", () => console.log("ciao"));
         div.innerHTML = content;
         div.style.top = "0";
         div.style.zIndex = "1000";
         div.style.position = "absolute";
         div.classList.add("w-full", "h-full");
-
         let containerHp = document.querySelector("#container-hp");
         containerHp.classList.add("hidden");
-
         document.body.appendChild(div);
+        switch (this.currentStep) {
+          case 1:
+            this.addEventsStep1();
+            break;
+          default:
+            console.log("non funziona");
+        }
       });
-  }
-  nextPage() {
+  };
+  addEventsStep1 = () => {
+    let img1Step1 = document.getElementById("img-step1-1");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let img2Step1 = document.getElementById("img-step1-2");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let img3Step1 = document.getElementById("img-step1-3");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let img4Step1 = document.getElementById("img-step1-4");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let img5Step1 = document.getElementById("img-step1-5");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let img6Step1 = document.getElementById("img-step1-6");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let img7Step1 = document.getElementById("img-step1-7");
+    img1Step1.addEventListener("click", this.saveChoice);
+    let btnNext = document.getElementById("btn-1");
+    btnNext.addEventListener("click", this.nextPage);
+  };
+  nextPage = () => {
+    // console.log("funziona");
     this.currentStep++;
-    this.fetchPage();
-  }
+    let contenutoStep = document.getElementById("container-step");
+    contenutoStep.classList.add("hidden");
+    this.openDesignTool();
+  };
 }
 
 let prova = new DesignTool();
-prova.fetchPage();
+// prova.openDesignTool();
 
 class Menu {
+  this;
   openMenu() {
     fetch("../menu/index.html")
       .then((res) => res.text())
@@ -125,6 +148,7 @@ class Menu {
         let doc = parser.parseFromString(myPage, "text/html");
         let div = document.createElement("div");
         let content = doc.querySelector("body").outerHTML;
+        div.id = "container-menu";
         div.innerHTML = content;
         div.style.top = "0";
         div.style.zIndex = "1000";
@@ -132,17 +156,34 @@ class Menu {
         div.classList.add("w-full", "h-full");
         let containerHp = document.querySelector("#container-hp");
         containerHp.classList.add("hidden");
-
         document.body.appendChild(div);
+        addEvents();
       });
   }
+  // openMenu() {
+  //   this.fetchMenu;
+  //   addEvents();
+  // }
+  closeMenu() {
+    // let containerHp = document.querySelector("#container-hp");
+    // containerHp.classList.remove("hidden");
+    // let contMenu = document.getElementById("container-menu");
+    // contMenu.classList.add("hidden");
+    // console.log("funzionava");
+    location.reload();
+  }
 }
-// let provaMenu = new Menu();
+function addEvents() {
+  let btnClose = document.getElementById("closeMenu");
+  // console.log(btnClose);
+  btnClose.addEventListener("click", provaMenu.closeMenu);
+}
+let provaMenu = new Menu();
 
-// let btnMenu = document.getElementById("navbar-text-menu");
-// console.log(btnMenu);
-// btnMenu.addEventListener("click", provaMenu.openMenu);
-
+let btnMenu = document.getElementById("navbar-text-menu");
+btnMenu.addEventListener("click", provaMenu.openMenu);
+let btnDesignTool = document.getElementById("navbar-text-dt");
+btnDesignTool.addEventListener("click", prova.openDesignTool);
 // prova.nextPage();
 // function nextStep() {
 //   fetch("../step2/index.html")
